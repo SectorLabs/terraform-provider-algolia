bb590f09cc6fea795c3dae75c5fa5ec8949cb4c4
Swen Kooij <swen@sectorlabs.ro>
2019-07-04 19:21:50 +0300

    Implement delete

--- resource_api_key_delete.go
+++ resource_api_key_delete.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"github.com/algolia/algoliasearch-client-go/algoliasearch"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
 // Deletes an existing API key
 func resourceAPIKeyDelete(d *schema.ResourceData, m interface{}) error {
+	client := *m.(*algoliasearch.Client)
+
+	_, err := client.DeleteAPIKey(d.Id())
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
 	return nil
 }
